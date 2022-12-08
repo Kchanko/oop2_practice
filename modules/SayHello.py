140c43222eac0c3798d8a8376cc053cb8eaae9bf
Kchanko <k21014kk@aitech.ac.jp>
2022-12-08 13:53:06 +0900

    最初のコミット

---
+++ modules/SayHello.py
@@ -0,0 +1,16 @@
+from ctypes.wintypes import tagRECT
+
+
+class SayHello:
+    def __init__(self, target="World"):
+        self.target = target
+
+    def say(self):
+        print(f"Hellom {self.target}!!")
+
+
+if __name__ == '__main__' :
+    app = SayHello()
+    app.say()
+    app = SayHello("Someone")
+    app.say()
